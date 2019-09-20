ee45636897139b5e038226c5b15359bce3ef7d91
Stephen Cross <stephen.cross@bristol.ac.uk>
2019-09-20 17:30:54 +0100

    Added track rendering using PIL
    Basic track renders using PIL.  This will be refined to give different colours for different tracks.

--- Sessions/Tracking/src/util.py
+++ Sessions/Tracking/src/util.py
@@ -12,10 +12,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import random
-import matplotlib.lines as lines
 
-from skimage import io
 from matplotlib.widgets import Slider
+from PIL import Image, ImageDraw
+from skimage import io
 
 
 ## Image loading
@@ -68,40 +68,69 @@ def load_coordinates(path):
 
 ## Create overlay
 def show_overlay(image, tracks):
+    # Getting the number of frames
+    n_frames = image.shape[2]
+
+    # Converting the image Numpy array to PIL image and adding overlay elements
+    image = render_overlay(image,tracks)
+    
     # Creating the figure
     fig, (ax_image, ax_slider) = plt.subplots(2,1, gridspec_kw={'height_ratios':[20,1],'bottom':0.05,'top':0.95})
         
     # Method to run each time slider is changed
     def update_image(val):
         ax_image.clear()
-        slice = int(round(val))
-        ax_image.imshow(image[:,:,slice])
-        draw_tracks(ax_image,tracks,frame)
+        frame = int(round(val))
+        ax_image.imshow(image[frame])
     
     # Creating the slider and running the image update for the first slice
-    slider = Slider(ax_slider,'Slice',0, image.shape[2]-1, valinit=0,valfmt="%0.0f")
+    slider = Slider(ax_slider,'Slice',0, n_frames-1, valinit=0,valfmt="%0.0f")
     slider.on_changed(update_image)
     update_image(0)
     
     # Displaying the image
     plt.show()
     
+## Converting the Numpy array to a PIL image and drawing tracks
+def render_overlay(image, tracks):
+    # Ensuring the tracks are sorted by frame
+    tracks = tracks.sort_values(by=['T'])
     
+    overlay_image = []
+    for frame in range(0,image.shape[2]-1):
+        print("Rendering frame ",frame)
+        # Converting to PIL image
+        overlay_image.append(Image.fromarray(image[:,:,frame]).convert('RGB'))
+        
+        # Adding overlay
+        draw_tracks(overlay_image[frame],tracks,frame)
+    
+    return overlay_image
+
 ## Draw tracks for a single frame
-def draw_tracks(ax, tracks,frame):
-    # Getting the row index of each coordinate in the previous frame
+def draw_tracks(image, tracks,frame):
+    # Getting tracks to display (only show last 20 frames)
+    tracks_to_show = tracks[:][(tracks['T'] > (frame-20)) & (tracks['T'] <=frame)]
+
+    # Getting the unique tracks  
+    track_IDs = tracks_to_show.TRACK_ID.unique()
     
-    # Iterating over each track in the current frame, adding it to the overlay
-    x1 = random.randint(0,100)
-    x2 = random.randint(0,100)
-    y1 = random.randint(0,100)
-    y2 = random.randint(0,100)
-    line = lines.Line2D([x1,x2],[y1,y2])
-    line.set_linewidth(1)
-    line.set_color('r')
-    ax.add_line(line)    
+    # Iterate over each track, drawing the path
+    for track_ID in track_IDs:
+        track = tracks_to_show[:][tracks_to_show['TRACK_ID'] == track_ID]
+        draw = ImageDraw.Draw(image) 
+        draw_track(draw, track)        
 
+def draw_track(draw, track):
+    for row in range(1,track.shape[0]-1):
+        x1 = track.X.values[[row-1]]
+        x2 = track.X.values[[row]]
+        y1 = track.Y.values[[row-1]]
+        y2 = track.Y.values[[row]]
 
+        draw.line((x1,y1,x2,y2), fill=(255,0,255), width=5)
+
+        
 ## Running these methods to check they work    
 path = "..\\data\\ExampleTimeseries.tif"
 image = load_images(path)
