9799ae9b29fb75c7a0e6ca8244b8a0b6fc8c443f
todd-fallesen <49306167+todd-fallesen@users.noreply.github.com>
2019-12-06 10:19:03 +0000

    File changes (unfortunately done on a laptop)

--- sessions/day01-image-processing-and-general-analysis/03_fiji-to-python/FIJI_circle_finder.py
+++ sessions/day01-image-processing-and-general-analysis/03_fiji-to-python/FIJI_circle_finder.py
@@ -1,13 +1,13 @@
 from ij import IJ #load the FIJI modules
 from ij.io import FileSaver
-from ij.io import OpenDialog  
+from ij.io import OpenDialog
 import os
 from ij.measure import ResultsTable
 
 IJ.run("Clear Results");  #clear results
 
-od = OpenDialog("Choose a file", None)  
-filename = od.getFileName()  
+od = OpenDialog("Choose a file", None)
+filename = od.getFileName()
 directory =  od.getDirectory()
 path = od.getPath()
 
@@ -19,11 +19,12 @@ imp = IJ.openImage(path)
 imp.show()
 #imp = IJ.getImage()
 IJ.run("8-bit");
+IJ.run("Invert");  #use this line if we need to invert the image
 IJ.run("Set Measurements...", "area min redirect=None decimal=3");
 IJ.run("Analyze Particles...", "display");
 
 #save the results table as a CSV
 #https://www.ini.uzh.ch/~acardona/fiji-tutorial/#s2
-save_string = directory + 'Results_test.csv'
+save_string = directory + 'Results_biology.csv'
 IJ.saveAs("Results", save_string);
-#table = ResultsTable.getResultsTable() 
+#table = ResultsTable.getResultsTable()
